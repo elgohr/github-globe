7bb5a2e84fd3cc71f17455e325d1cc92174832da
Lars Gohr <lars@gohr.digital>
2023-04-01 21:27:19 +0200

    :recycle: Enable possibility for preloading

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import time
 
-from geojson import Point, Feature, dumps, FeatureCollection
+from geojson import Point, Feature, dumps, FeatureCollection, loads
 from geopy import Nominatim
 from geopy.exc import GeopyError
 from github import Github, RateLimitExceededException
@@ -13,36 +13,64 @@ def collect(token, user):
     nn = Nominatim(user_agent="github globe generator")
     base_user = get_user(gh, user)
     repos = get_repos(base_user)
-    locations = set()
+
+    locations = {}
+    user_locations = {}
     location_details = set()
+
+    if os.path.isfile("global_usage.json"):
+        with open("global_usage.json", 'r') as data_file:
+            features = FeatureCollection(loads(data_file.read())).get("features")
+            if features is not None:
+                for feature in features:
+                    properties = feature.get("properties")
+                    if properties is not None:
+                        name = properties.get("name")
+                        location = properties.get("location")
+                        if name is not None and location is not None:
+                            user_locations[name] = location
+                            geo = feature.get("geometry")
+                            if geo is not None:
+                                coordinates = geo.get("coordinates")
+                                if len(coordinates) == 2:
+                                    locations[name] = Point((coordinates[0], coordinates[1]))
+
     for repo in repos:
         print("checking: " + repo.name)
         gh_deps_info = GithubDependentsInfo(repo.full_name)
         gh_deps_info.collect()
         for package in gh_deps_info.packages:
             for dependent in package["public_dependents"]:
-                repo_user = get_user(gh, dependent["name"].split("/")[0])
-                if repo_user.location is not None:
+                user_name = dependent["name"].split("/")[0]
+                location = ""
+                if user_name in user_locations:
+                    location = user_locations[user_name]
+                else:
+                    repo_user = get_user(gh, user_name)
+                    if repo_user.location is not None:
+                        location = repo_user.location
+                if location:
                     if any(c.isalpha() for c in repo_user.location):
                         if repo_user.location not in locations:
-                            locations.add(repo_user.location)
                             try:
-                                location = nn.geocode(repo_user.location)
+                                locations[repo_user.location] = nn.geocode(repo_user.location)
                             except GeopyError:
                                 print("ignoring:" + repo_user.location)
-                                location = None
-                            if location is not None:
-                                loc = Location(repo_user.location, location.latitude, location.longitude)
-                                location_details.add(loc)
+                        if repo_user.location in locations:
+                            location = locations[repo_user.location]
+                            loc = Usage(user_name, repo_user.location, location.latitude, location.longitude)
+                            location_details.add(loc)
 
     features = []
-    for location in location_details:
-        p = Point((location.longitude, location.latitude))
-        features.append(Feature(geometry=p, properties={"location": location.name}))
+    for usage in location_details:
+        p = Point((usage.longitude, usage.latitude))
+        features.append(Feature(geometry=p, properties={
+            "name": usage.name,
+            "location": usage.location,
+        }))
 
-    f = open("global_usage.json", "w")
-    f.write(dumps(FeatureCollection(features)))
-    f.close()
+    with open("global_usage.json", 'w') as data_file:
+        data_file.write(dumps(FeatureCollection(features)))
 
 
 def get_repos(base_user):
@@ -61,9 +89,10 @@ def get_user(gh, user):
         get_user(gh, user)
 
 
-class Location:
-    def __init__(self, name, latitude, longitude):
+class Usage:
+    def __init__(self, name, location, latitude, longitude):
         self.name = name
+        self.location = location
         self.latitude = latitude
         self.longitude = longitude
 
